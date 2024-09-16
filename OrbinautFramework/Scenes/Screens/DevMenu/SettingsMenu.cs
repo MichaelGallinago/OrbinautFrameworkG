d115ce3b2877e2cb7301ab5830483fb8cb71a477
Michael Gallinago <michael.gallinago@gmail.com>
2024-09-16 20:41:03 +0500

    New events

--- OrbinautFramework/Scenes/Screens/DevMenu/SettingsMenu.cs
+++ OrbinautFramework/Scenes/Screens/DevMenu/SettingsMenu.cs
@@ -1,5 +1,7 @@
 using Godot;
+using OrbinautFramework3.Audio.Player;
 using OrbinautFramework3.Framework;
+using OrbinautFramework3.Framework.InputModule;
 using OrbinautFramework3.Framework.StaticStorages;
 
 namespace OrbinautFramework3.Scenes.Screens.DevMenu;
@@ -7,9 +9,12 @@ namespace OrbinautFramework3.Scenes.Screens.DevMenu;
 public partial class SettingsMenu : Menu
 {
 	private static void OnExit() => ConfigUtilities.Save();
-
-	private void OnTargetFpsChanged(ushort value) => Settings.TargetFps = value;
-	private void OnWindowScaleChanged(byte scale) => Settings.WindowScale = scale;
-	private void OnVSyncSwitched(long mode) => Settings.VSyncMode = (DisplayServer.VSyncMode)mode;
-	private void OnWindowModeSwitched(long mode) => Settings.WindowMode = (DisplayServer.WindowMode)mode;
+	
+	private static void OnJoypadRumbleChanged(bool isEnabled) => InputUtilities.JoypadRumble = isEnabled;
+	private static void OnMusicVolumeChanged(float volume) => AudioPlayer.Music.Volume = volume;
+	private static void OnSoundVolumeChanged(float volume) => AudioPlayer.Sound.Volume = volume;
+	private static void OnTargetFpsChanged(ushort value) => Settings.TargetFps = value;
+	private static void OnWindowScaleChanged(byte scale) => Settings.WindowScale = scale;
+	private static void OnVSyncSModeChanged(long mode) => Settings.VSyncMode = (DisplayServer.VSyncMode)mode;
+	private static void OnWindowModeChanged(long mode) => Settings.WindowMode = (DisplayServer.WindowMode)mode;
 }
